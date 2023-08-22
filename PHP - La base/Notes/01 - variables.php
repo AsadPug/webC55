dc7158fd988417213ca3d833fbdee41e7b6cb18d
AsadPug <97375660+AsadPug@users.noreply.github.com>
2023-08-22 09:27:13 -0400

    ds

---
+++ PHP - La base/Notes/01 - variables.php
@@ -0,0 +1,20 @@
+<?php
+	echo "salut!";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Variables</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		Nom : 
+	</body>
+</html>
+
+
+
+
+
+
+
